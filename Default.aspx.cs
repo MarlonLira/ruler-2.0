b37773e3c25ad93089853d44d5021b02d4842cf6
Marlon Lira <marlonlira3@gmail.com>
2020-02-17 12:38:02 -0300

    test(LoadEntities)

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -2,6 +2,7 @@
 using Ruler.Controllers;
 using Ruler.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI;
 
@@ -12,17 +13,18 @@ namespace Ruler
   {
     protected void Page_Load(object sender, EventArgs e)
     {
-      var cl = new ClientController();
-      var b = cl.Save(new Client() { Name = "Marlon", Phone = "81985665252", RegistryCode = "02536547752" });
-      //var r = cl.Search(1);
+      LoadEntities();
+      //var cl = new ClientController();
+      //var b = cl.Save(new Client() { Name = "Marlon", Phone = "81985665252", RegistryCode = "02536547752" });
+      ////var r = cl.Search(1);
 
-      var p1 = new ProductController();
-      var a = p1.Save(new Product()
-      {
-        Name = "Notebook",
-        Brand = "Dell",
-        ManufactDate = DateTime.UtcNow.AddHours(-3)
-      });
+      //var p1 = new ProductController();
+      //var a = p1.Save(new Product()
+      //{
+      //  Name = "Notebook",
+      //  Brand = "Dell",
+      //  ManufactDate = DateTime.UtcNow.AddHours(-3)
+      //});
 
       //Response.Redirect("HomePge.aspx", false);
 
@@ -31,5 +33,25 @@ namespace Ruler
     public Client client { get { return new ClientController().Search(1).FirstOrDefault(); } }
     public Product product { get { return new ProductController().Search(2).FirstOrDefault(); } }
     public string Teste { get { return "testando"; } }
+
+    private void LoadEntities()
+    {
+      if (new ClientController().Search(1).FirstOrDefault() == null && new ClientController().Search(2).FirstOrDefault() == null)
+      {
+        IList<Client> Clients = new List<Client>();
+        new ClientController().Save(new Client() { Name = "Marlon Lira", Phone = "81985445636", RegistryCode = "08565425875" });
+        new ClientController().Save(new Client() { Name = "Arthur Bernardo", Phone = "81977545886", RegistryCode = "05265852875" });
+        new ClientController().Save(new Client() { Name = "Gustavo Gusmão", Phone = "81966444436", RegistryCode = "08798425255" });
+        new ClientController().Save(new Client() { Name = "Felipe Seabra", Phone = "81965445786", RegistryCode = "08896325857" });
+      }
+      if (new ProductController().Search(2).FirstOrDefault() == null && new ProductController().Search(1).FirstOrDefault() == null) { 
+        IList<Product> Products = new List<Product>();
+
+        new ProductController().Save(new Product() { Name = "Iphone 10", Category = "Smartphone", Price = 7000d, Brand = "Apple", Lot = "XL00001", RegistryCode = "GTH88866LKH" });
+        new ProductController().Save(new Product() { Name = "Redmi Note 8", Category = "Smartphone", Price = 1500d, Brand = "Xiaomi", Lot = "PL05501", RegistryCode = "GUI899666LBR" });
+        new ProductController().Save(new Product() { Name = "Galaxy S8", Category = "Smartphone", Price = 2200d, Brand = "Samsung", Lot = "AX08901", RegistryCode = "LOP00366ERR" });
+        new ProductController().Save(new Product() { Name = "Zenfone 5", Category = "Smartphone", Price = 2200d, Brand = "Asus", Lot = "AS099023", RegistryCode = "POI08796EUY" });
+      }
+    }
   }
 }
--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 ﻿namespace Ruler.Migrations
 {
-  using System;
-  using System.Data.Entity;
+    using Ruler.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
   using System.Data.Entity.Migrations;
   using System.Linq;
 
@@ -15,12 +17,23 @@
 
     protected override void Seed(Context.RulerContext context)
     {
-      //  This method will be called after migrating to the latest version.
+      IList<Client> Clients = new List<Client>();
+      IList<Product> Products = new List<Product>();
 
-      //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-      //  to avoid creating duplicate seed data.
+      Clients.Add(new Client() { Name = "Marlon Lira", Phone = "81985445636", RegistryCode = "08565425875" });
+      Clients.Add(new Client() { Name = "Arthur Bernardo", Phone = "81977545886", RegistryCode = "05265852875" });
+      Clients.Add(new Client() { Name = "Gustavo Gusmão", Phone = "81966444436", RegistryCode = "08798425255" });
+      Clients.Add(new Client() { Name = "Felipe Seabra", Phone = "81965445786", RegistryCode = "08896325857" });
 
-      
+      Products.Add(new Product() { Name = "Iphone 10", Category = "Smartphone", Price = 7000d, Brand="Apple", Lot="XL00001",RegistryCode="GTH88866LKH" });
+      Products.Add(new Product() { Name = "Redmi Note 8", Category = "Smartphone", Price = 1500d, Brand="Xiaomi", Lot="PL05501",RegistryCode="GUI899666LBR" });
+      Products.Add(new Product() { Name = "Galaxy S8", Category = "Smartphone", Price = 2200d, Brand="Samsung", Lot="AX08901",RegistryCode="LOP00366ERR" });
+      Products.Add(new Product() { Name = "Zenfone 5", Category = "Smartphone", Price = 2200d, Brand="Asus", Lot="AS099023",RegistryCode="POI08796EUY" });
+
+      context.Clients.AddRange(Clients);
+      context.Products.AddRange(Products);
+
+      base.Seed(context);
     }
   }
 }
